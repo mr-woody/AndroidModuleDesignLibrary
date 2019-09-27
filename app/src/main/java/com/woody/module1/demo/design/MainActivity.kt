10bdc95977fe2ca9721526d2495ce8ea1074e7b6
tao.yue <12345678>
2019-09-27 11:38:29 +0800

    完善模块化新方案demo

--- app/src/main/java/com/woody/module1/demo/design/MainActivity.kt
+++ app/src/main/java/com/woody/module1/demo/design/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.woody.module1.demo.design
 
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.okay.router.Router
+import com.okay.router.callback.RouteCallback
+import com.okay.router.configs.RouterConfiguration
+import com.okay.router.exception.NotFoundException
+import com.okay.router.module.RouteRule
 import com.woody.module1.demo.R
 import com.woody.module1.demo.config.ModuleConfig
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,6 +18,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        RouterConfiguration.get().callback = object: RouteCallback {
+            override fun notFound(uri: Uri?, e: NotFoundException?) {
+                Toast.makeText(applicationContext,e?.message, Toast.LENGTH_LONG).show()
+            }
+
+            override fun onOpenSuccess(uri: Uri?, rule: RouteRule<out RouteRule<*, *>, *>?) {}
+
+            override fun onOpenFailed(uri: Uri?, e: Throwable?) {}
+
+        }
 
         btn1.setOnClickListener {
             Router.create(ModuleConfig.Module1.URL_MODULE_MAIN_ACTIVITY)
@@ -27,9 +44,6 @@ class MainActivity : AppCompatActivity() {
         // BaseLibrary
         // CommonComponent
         // Provider
-
-
-
 
 
         // Business Layer
