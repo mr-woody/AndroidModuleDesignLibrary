088676835ee8952e5ca8167a72cba88d2a334d8b
tao.yue <12345678>
2020-10-16 11:16:15 +0800

    1.代码规约配置

--- app/src/main/java/com/woody/module1/demo/application/MainApplication.java
+++ app/src/main/java/com/woody/module1/demo/application/MainApplication.java
@@ -22,9 +22,7 @@ public class MainApplication implements IApplicationLife {
 
     @Override
     public void onCreate(Application application) {
-
         Log.e(TAG,"onCreate ...");
-
     }
 
     @Override
