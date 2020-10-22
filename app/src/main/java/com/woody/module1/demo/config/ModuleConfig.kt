c9cafe52f4d27c9391972310af771e5889f8e303
tao.yue <12345678>
2020-10-22 16:15:00 +0800

    1.测试代码123

--- app/src/main/java/com/woody/module1/demo/config/ModuleConfig.kt
+++ app/src/main/java/com/woody/module1/demo/config/ModuleConfig.kt
@@ -13,6 +13,7 @@ class ModuleConfig {
         const val HOST = "${SCHEME}://scheme.woody.module1/"
         const val PACKAGE = "com.woody.module1"
 
+
         //activity
         const val URL_MODULE_MAIN_ACTIVITY = HOST + "module1/page/main"
 
